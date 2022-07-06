136f977e477b845b35f6d3ea8c9c9c525fb4f7d2
unknown <fleiras18@gmail.com>
2022-07-06 12:46:28 -0300

    commit Deploy

--- index.js
+++ index.js
@@ -20,11 +20,12 @@
 const server = require("./src/app.js");
 const { apiInfo } = require("./src/Controllers/utils.js");
 const { conn } = require("./src/db.js");
+const { PORT } = process.env;
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
-    console.log("%s listening at 3001"); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
     apiInfo();
   });
 });
